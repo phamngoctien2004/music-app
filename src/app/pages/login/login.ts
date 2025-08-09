8742b4dbd19e2261085232fbc5d8d98460659cd6
tiendevfs <tienhvnhgpt@gmail.com>
2025-08-09 17:23:52 +0700

    login and signup

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import {PasswordModule} from 'primeng/password';
 import {InputText} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [PasswordModule, InputText],
+  imports: [PasswordModule, InputText, FormsModule, RouterLink],
   templateUrl: './login.html',
   standalone: true,
   styleUrl: './login.css'
 })
 export class Login {
+  email: String = '';
 
+  onSubmit(){
+    console.log('ok')
+  }
 }
--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, input, model, signal} from '@angular/core';
+import {StepperModule} from 'primeng/stepper';
+import {FormsModule} from '@angular/forms';
+import {InputText} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {Password} from 'primeng/password';
+import {RouterLink} from '@angular/router';
+import {InputMask} from 'primeng/inputmask';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    StepperModule,
+    FormsModule,
+    InputText,
+    ButtonModule,
+    Password,
+    RouterLink,
+    InputMask
+  ],
   templateUrl: './signup.html',
+  standalone: true,
   styleUrl: './signup.css'
 })
 export class Signup {
+  activeStep = model<number>(1);
 
+
+  emailStep = model<number>(1)
+  passwordStep=  model<number>(2)
+  doneStep =  model<number>(3)
+
+  // validate
+  passwordPattern = input<string>('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$');
+  password = '';
+  email: string = '';
+  birth: String ='';
+  gender: String = '';
+  signup(){
+    console.log("sign")
+  }
 }
